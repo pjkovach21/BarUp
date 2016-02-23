87ec2c6578137819e8756fac171df7f4c209622f
Peter Kovach <pjkovach21@gmail.com>
2016-02-23 17:40:55 -0500

    playing around with searching for time

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,2 +1,46 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	$(".js-event-date").on("change", function(){
+		var eventDate = $(".js-event-date").val();
+		console.log(eventDate)
+		filterEvents(eventDate)
+	});
+});
+
+
+function filterEvents(eventDate){
+	// var today = new Date();
+	// var tomorrow = new Date(new Date(now).setDate(now.getDate() + 1));
+	// var nextWeek = new Date(new Date(now).setDate(now.getDate() + 7));
+	// var nextMonth = new Date(new Date(now).setMonth(now.getMonth() + 1));
+
+	console.log("filtering events")
+	$.ajax ({
+		url: "event/select",
+		success: function(response){
+			console.log(response);
+
+			$(".js-event-list").empty();
+			var eventNewArray=response.filter(
+				function (events){
+					console.log(eventDate)
+					return events.date === eventDate
+				});
+			console.log(eventNewArray)
+			if (eventNewArray.length!==0){
+				eventNewArray.forEach (function(event){
+
+					$(".js-event-list").append(`<li>${event.title}</li>`);
+
+				});
+			}else{
+				alert("No events on this search")
+			}
+			},
+			error: function(){
+				console.log("shit hit the fan")
+			}
+	});
+}
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,30 @@ class EventsController < ApplicationController
 
 	def index
 		@events = Event.all
+
+		@today = DateTime.today.beginning_of_day..DateTime.today.end_of_day
+
+		nextday = Date.today + 1.day
+		@tomorrow = nextday.beginning_of_day..nextday.end_of_day
+
+		@this_week = Date.today..Date.today.at_end_of_week
+
+		@this_month = Date.today..Date.today.at_end_of_month
+
+		
+		
+		# today_range = Date.today.beginning_of_day..Date.today.end_of_day
+  #    	@today=@events.where(start_date: today_range)
+		
+		# nextday = Date.today + 1.day
+		# tomorrow_range = nextday.beginning_of_day..nextday.end_of_day
+		# @tomorrow = @events.where(start_date: tomorrw_range)
+
+		# week_range = Date.today..Date.today.at_end_of_week
+  #    	@this_week=@events.where(start_date: week_range)
+     	
+  #    	month_range = Date.today..Date.today.at_end_of_month
+  #    	@this_month = @events.where(start_date: month_range)
 	end
 
 	def edit
@@ -25,6 +49,12 @@ class EventsController < ApplicationController
       redirect_to(events_path)
     end
   end
+
+  	def event
+		@event = Event.all
+
+		render json: @event
+	end
 
 	def destroy
 
