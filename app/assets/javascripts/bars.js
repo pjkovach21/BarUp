77e0eb57c84af23c6b24c8d5518be0efd4416b8b
Peter Kovach <pjkovach21@gmail.com>
2016-02-23 11:31:03 -0500

    able to sort bars

--- app/assets/javascripts/bars.js
+++ app/assets/javascripts/bars.js
@@ -17,11 +17,21 @@ function filterBars(barType){
 		success: function(response){
 			console.log(response);
 			$(".js-bar-list").empty();
-			response.forEach(function(bar){
-				
-				$(".js-bar-list").append(`<li>${bar.bar}</li>`)
 
-			})
+            var barNewArray=response.filter(
+				function (bars){
+				   return bars.bar_type===barType
+				})
+			console.log(barNewArray)
+			if (barNewArray.length!==0){
+	            barNewArray.forEach (function(bar){
+	            	$(".js-bar-list").append(`<li>${bar.bar}</li>`);
+	           
+	            });
+	        }else{
+	        	alert("There are no bars in this category");
+	        }
+
 		},
 		error: function(){
 			console.log("something went wrong");
